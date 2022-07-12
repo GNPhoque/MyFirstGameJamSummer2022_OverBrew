5cf0e1ae828db629a2fc4d80ce889616d8dca7c9
Saiti91 <106150763+Saiti91@users.noreply.github.com>
2022-07-12 10:48:34 +0200

    10:48

--- Assets/Scripts/Script Item/CraftingBox.cs
+++ Assets/Scripts/Script Item/CraftingBox.cs
@@ -3,14 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CraftingBox : MonoBehaviour, IInteractable
-{ 
+{
     [SerializeField] private float _craftingTimeStandingStill;
     [SerializeField] private float _craftingTime;
+    [SerializeField] private RecipeScriptableObject craftingRecipe;
+    [SerializeField] private List<Ingredient> currentIngredients;
+
     private float _craftingRealTime;
     private bool _crafting;
-    [SerializeField] private RecipeScriptableObject craftingRecipe;
-
-    [SerializeField] private List<Ingredient> currentIngredients;
 
     PlayerInputActions playerInputActions;
 
@@ -44,41 +44,38 @@ public class CraftingBox : MonoBehaviour, IInteractable
                 currentIngredients.Add(GameManager.instance.Ingredient);
                 GameManager.instance.Ingredient = Ingredient.None;
             }
-
             //check si l'ingredient est compatible avec la table de craft et demarrer le timer sauf pour le grinder
-           
-
             //et dans ce timer verifier une fois le timer terminé quel est le resultat de la transformation
             //il peut recupéré l'ingredients
             //et que l'action est finie alors resultat bouillie;
-
-
-            
         }
-        if(GameManager.instance.Ingredient == Ingredient.None)
+        if (GameManager.instance.Ingredient == Ingredient.None)
         {
-            if(currentIngredients.Count == craftingRecipe.ingredients.Count)
+            if (currentIngredients != null && craftingRecipe!=null)
             {
-                foreach (Ingredient ingredient in currentIngredients)
+                if (currentIngredients.Count == craftingRecipe.ingredients.Count)
                 {
-                    if (craftingRecipe.ingredients.Contains(ingredient))
+                    foreach (Ingredient ingredient in currentIngredients)
                     {
-                        _crafting = true;
+                        if (craftingRecipe.ingredients.Contains(ingredient))
+                        {
+                            _crafting = true;
+                        }
                     }
                 }
-            }
-            else
-            {
-                Debug.Log("Pas de recette avec ces éléments");
+                else
+                {
+                    Debug.Log("Pas de recette avec ces éléments");
+                }
             }
         }
 
 
-        if (GameManager.instance.Ingredient == Ingredient.None && currentIngredients[0] != Ingredient.None)
+        /*if (GameManager.instance.Ingredient == Ingredient.None && currentIngredients[0] != Ingredient.None)
         {
             GameManager.instance.Ingredient = currentIngredients[0];
 
-        }
+        }*/
 
 
         /*if (GameManager.instance.Ingredient == Ingredient.None && currentIngredient == Ingredient.HealingPotion)
@@ -90,6 +87,7 @@ public class CraftingBox : MonoBehaviour, IInteractable
     ///si ingredient. ... != null alors transformation
     ///si ingredient. ..!= null && craftingbox.ingredient != null alors tranformatioon en un item
     ///si transformation fini alors on recupere l'item
+
     private void Update()
     {
         if (_crafting)
